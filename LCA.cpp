bad8c28238f4e77e2d2b9b2eac7ce57a91e187e3
Little <108563712+LittleNoPro@users.noreply.github.com>
2022-07-15 15:50:00 +0700

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,29 +1,39 @@
+#define MAX   100100
+#define LOG   17
 int numNode;
-vector<int> adj[MAX]; // mảng danh sách kề 
-int par[MAX]; // mảng lưu cha của các nút
-int high[MAX]; // mảng lưu độ sâu của các nút (gốc cây có độ sâu là 0)
+vector<int> adj[MAX]; // danh sách kề
+int par[MAX][LOG + 1]; // mảng cha
+int high[MAX]; // số cạnh từ một đỉnh tới gốc (độ sâu)
 
 void dfs(int u) {
-	for (int v : adj[u]) if (v != par[u])) {
-		par[v] = u;
+	for (int v : adj[u]) if (v != par[u][0]) {
+		par[v][0] = u; // cha trực tiếp của v là u
 		high[v] = high[u] + 1;
 		dfs(v);
 	}
 }
-dfs(1);
 
-int lcaSlow(int u, int v) { // tìm tổ tiên chung gần nhất giữa u và v
-	// nếu v có độ sâu lớn hơn u, đổi chỗ u và v để ta luôn có thể giả sử u có độ sâu lớn hơn hoặc bằng v.
-	if (high[v] > high[u]) return lcaSlow(v, u);
+// chuẩn bị ban đầu
+high[0] = -1; // rất quan trọng, không thể bỏ sót
+dfs(1);
+for (int j = 1; j <= LOG; j++) for (int i = 1; i <= numNode; i++)
+	par[i][j] = par[par[i][j - 1]][j - 1];
+
+int lca(int u, int v) {
+	// nếu v có độ sâu lớn hơn u, đổi chỗ u và v
+	if (high[v] > high[u]) return lca(v, u);
 	
-	// cân bằng độ sâu: di chuyển nút u lên trên cho đến khi có độ sâu bằng v
-	while (high[u] > high[v]) u = par[u];
+	// bước 1: nhảy u lên để u có độ sâu bằng v
+	for (int j = LOG; j >= 0; j--) if (par[u][j] >= par[v]) u = par[u][j];
 	
-	// di chuyển u và v cùng lúc cho đến khi hai nút gặp nhau 
-	while (u != v) {
-		u = par[u];
-		v = par[v];
+	// nếu sau bước 1, hai đỉnh u và v đã bằng nhau rồi, kết luận đây là lca
+	if (u == v) return u;
+
+	// bước 2: nhảy đồng thời u và v đến vị trí xa nhất mà chưa gặp nhau
+	for (int j = LOG; j >= 0; j--) if (par[u][j] != par[v][j]) {
+		u = par[u][j]; v = par[v][j];
 	}
 
-	return u; // tổ tiên chung gần nhất chính là điểm gặp nhau đầu tiên
+	// nhảy thêm một bước nữa là tới lca
+	return par[u][0];
 }
