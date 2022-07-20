096f8d1373e81944f3296676b0e11ed6da3e1623
Little <108563712+LittleNoPro@users.noreply.github.com>
2022-07-20 09:04:30 +0700

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -13,53 +14,56 @@ using vpll = vector<pll>;
 #define pf push_front
 #define MAX   100100
 #define LOG   17
+
+long long n, k;
 int numNode;
 vector<int> adj[MAX]; 
 int par[MAX][LOG + 1]; 
 int high[MAX]; 
 
 void dfs(int u) {
-	for(int v : adj[u]) 
-		if v != par[u][0]) {
-			par[v][0] = u; // cha trực tiếp của v là u
-			high[v] = high[u] + 1;
-			dfs(v);
-		}
+    for(int v : adj[u]) 
+        if(v != par[u][0]) {
+            par[v][0] = u; 
+            high[v] = high[u] + 1;
+            dfs(v);
+        }
+}
+
+void prepare() {
+    high[0] = -1; 
+    dfs(1);
+    for(int j = 1; j <= LOG; j++) 
+        for(int i = 1; i <= n; i++)
+            par[i][j] = par[par[i][j - 1]][j - 1];
 }
 
 int lca(int u, int v) {
-	// nếu v có độ sâu lớn hơn u, đổi chỗ u và v
-	if(high[v] > high[u]) 
-		return lca(v, u);
-	
-	// bước 1: nhảy u lên để u có độ sâu bằng v
-	for(int j = LOG; j >= 0; j--) 
-		if (par[u][j] >= par[v]) 
-			u = par[u][j];
-	
-	// nếu sau bước 1, hai đỉnh u và v đã bằng nhau rồi, kết luận đây là lca
-	if(u == v) 
-		return u;
-
-	// bước 2: nhảy đồng thời u và v đến vị trí xa nhất mà chưa gặp nhau
-	for(int j = LOG; j >= 0; j--) 
-		if (par[u][j] != par[v][j]) {
-			u = par[u][j]; 
-			v = par[v][j];
-	}
-
-	// nhảy thêm một bước nữa là tới lca
-	return par[u][0];
+    if(high[v] > high[u]) 
+        return lca(v, u);
+    for(int j = LOG; j >= 0; j--) 
+        if(high[par[u][j]] >= high[v]) 
+            u = par[u][j];
+    if(u == v) 
+        return u;
+    for(int j = LOG; j >= 0; j--) 
+        if(par[u][j] != par[v][j]) {
+            u = par[u][j]; 
+            v = par[v][j];
+    }
+    return par[u][0];
 }
 
 void solve() {
-	high[0] = -1; 
-	dfs(1);
-	for(int j = 1; j <= LOG; j++) 
-		for(int i = 1; i <= numNode; i++)
-			par[i][j] = par[par[i][j - 1]][j - 1];
-	
-	// code ... 
+    cin >> n;
+    for(int i = 1; i < n; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    prepare();
+    
 } 
 
 int main(){
