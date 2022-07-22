59891d8b1303a3dc1df4cab0df684bbfc00f6d6f
Little <108563712+LittleNoPro@users.noreply.github.com>
2022-07-22 10:16:28 +0700

    Create SieveOnRange.cpp

---
+++ SieveOnRange.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void sieve(ll l, ll r) {
+    vector<bool> prime(r - l + 1, true);
+    for(ll i = 2; i <= sqrt(r); i++) 
+        for(ll j = max(i * i, (l + i - 1) / i * i); j <= r; j += i) 
+            prime[r - l] = false;
+    for(ll i = max(l, 2ll); i <= r; i++) 
+        if(prime[i - l])
+            cout << i << " ";
+}
+
+int main() {
+  
+}
