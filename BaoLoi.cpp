f953398bbafd1ed160670e38b104d1a3348af9b9
Little <108563712+LittleNoPro@users.noreply.github.com>
2022-12-10 10:37:25 +0700

    Update BaoLoi.cpp

--- BaoLoi.cpp
+++ BaoLoi.cpp
@@ -1,23 +1,30 @@
-// Minh Tú 
-
 #include <bits/stdc++.h>
+#pragma ("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
 using namespace std;
 #define ll  long long
 #define db  double
-#define pll  pair<ll, ll>
+#define str string
 #define vl  vector<ll>
-#define vb  vector<bool>
+#define pii pair<int, int>
+#define pll  pair<ll, ll>
+#define vpii vector<pii>
 #define vpll  vector<pll>
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sz(x) x.size()
 #define mp make_pair
+#define pb push_back
 #define fi first
 #define se second
-#define MASK(i) (1LL << (i))
 #define sqr(i) i * i
-#define pb push_back
-#define pf push_front
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
+#define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
+#define turn_on(i, m) (m |= (1LL << i))
+#define turn_off(i, m) (m &= ~(1LL << i))
+#define MASK(i) (1ll << (i))
+#define BIT(x, i) (((x) >> (i)) & 1) 
+int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 const db pi = acos(1);
 const int MOD = 1e9 + 19972207;
 const ll INF = 1e18;
@@ -48,12 +55,12 @@ db RAD_TO_DEG(db d) {
     return d * 180.0 / pi;
 }
 
-point rotation(point p, db alpha) { // quay diem p 1 goc alpha ma goc la O
+point rotation(point p, db alpha) {
     db rad = DEG_TO_RAD(alpha);
     return point(p.x * cos(rad) - p.y * sin(rad), p.x * sin(rad) + p.y * cos(rad));
 }
 
-point rotations(point p1, point p2, db alpha) { // quay diem p2 quanh diem p1 1 goc alpha
+point rotations(point p1, point p2, db alpha) {
     db rad = DEG_TO_RAD(alpha);
     point p3, p4;
     p3.x = p1.x - p2.x;
@@ -66,7 +73,7 @@ struct line {
     db a, b, c;
 };
 
-line makeLine(point p1, point p2) { 
+line makeLine(point p1, point p2) {
     line l1;
     if(p1.x == p2.x) {
         l1.a = 1;
@@ -139,26 +146,74 @@ db cross(vect v1, vect v2) { // tich co huong 2 vector v1 va v2
     return (v1.x * v2.y - v1.y * v2.x);
 }
 
-db distToLine(point p, point a, point b) { // khoang cach ngan nhat tu p den duong thang ab
+db getLength_sq(vect v) {
+    return v.x * v.x + v.y * v.y;
+}
+
+db distToLine(point p, point a, point b) {
     vect AP = getVect(a, p), AB = getVect(a, b);
-    db k = scalar(AP, AB) / sqrLen(AB);
+    db k = scalar(AP, AB) / getLength_sq(AB);
     point c = translate(a, scale(AB, k));
     return dist(p, c);
 }
 
-db distToLineSegment(point p, point a, point b) { // khoang cach ngan nhat tu p den doan thang ab
+db distToLineSegment(point p, point a, point b) {
     vect AP = getVect(a, p), AB = getVect(a, b);
-    db k = scalar(AP, AB) / sqrLen(AB);
-    if(k < 0.0) 
+    db k = scalar(AP, AB) / getLength_sq(AB);
+    if(k < 0.0) {
         return dist(p, a);
-    if(k > 1.0) 
+    }
+    if(k > 1.0) {
         return dist(p, b);
+    }
     return distToLine(p, a, b);
 }
 
+void FindFootOfTheAltitude(point A, point B, point C, point &cur) { // Tim toa do chan duong cao
+    line l = makeLine(B, C);
+    double ans = -(l.a * A.x + l.b * A.y + l.c) / (l.a * l.a + l.b * l.b);
+    cur.x = ans * l.a + A.x;
+    cur.y = ans * l.b + A.y;
+}
+
+db slope(point A, point B) { // Tim he co so
+    if(B.x - A.x != 0)
+        return (B.y - A.y) / (B.x - A.x);
+    return 1e8;
+}
+
+void FindIntersect(point A, point B, point C, point D, point &cur) {   // Tim toa do giao diem cua 2 doan thang
+    long double a1 = B.y - A.y;
+    long double b1 = A.x - B.x;
+    long double c1 = a1 * (A.x) + b1 * (A.y);
+    long double a2 = D.y - C.y;
+    long double b2 = C.x - D.x;
+    long double c2 = a2 * (C.x) + b2 * (C.y);
+    long double determinant = a1*b2 - a2*b1;
+    if(determinant == 0) {
+        cur.x = FLT_MAX;
+        cur.y = FLT_MAX;
+    }
+    else {
+        cur.x = 1.0*(b2*c1 - b1*c2)/determinant;
+        cur.y = 1.0*(a1*c2 - a2*c1)/determinant;
+    }
+}
+
+// int CCW(point A, point B, point C) {
+//     vect AB = getVect(A, B);
+//     vect AC = getVect(A, C);
+//     if(cross(AB, AC) > 0) 
+//         return 1;  // diem C nam phia tren doan thang AB
+//     else
+//     if(cross(AB, AC) < 0) 
+//         return -1; // diem C nam phia duoi doan thang AB
+//     return 0; 
+// }
+
 map<pll, ll> res;
-ll n, m;
-point p[MAXN], q[MAXN], A;
+int n, m;
+point q[MAXN], p[MAXN], A;
 
 bool CCW(const point& D, const point& E, const point& F){
     return cross(E - D, F - E) > 0;
@@ -179,6 +234,15 @@ void BuildConvexHull(){
             m--;
         q[m++] = p[i];
     }
+}
+
+db AreaOfTriangle(point c, point a, point b) {
+    return ((db) 1 / 2 * distToLineSegment(c, a, b) * dist(a, b));
+}
+
+db quadrilateral(const point& A, const point& B, const point& C, const point& D){
+    return abs(((A.x * B.y+B.x * C.y+C.x * D.y+D.x * A.y) - 
+        (A.y * B.x+B.y * C.x+C.y * D.x+D.y * A.x))) / 2;
 }
 
 void solve() {
@@ -216,11 +280,12 @@ void solve() {
         cur.pb(tmp);
     }
     sort(cur.begin(), cur.end());
-    for(int i = 0; i < m; i++)
-        cout << cur[i] << " ";
-}   
+    for(int i = 0; i < m; i++) {
+        cout << q[i].x << " " << q[i].y << '\n';
+    }
+}
 
-int main(){
+int main(){ 
     IOS;
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
