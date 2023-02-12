45ebbde10ab17b8c932ab5d79133d3bc755437bd
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-02-12 19:03:40 +0700

    Update TopoSort.cpp

--- TopoSort.cpp
+++ TopoSort.cpp
@@ -1,36 +1,3 @@
-// Minh Tú 
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll  long long
-#define db  double
-#define pll  pair<ll, ll>
-#define pii pair<int, int>
-#define vpii vector<pii>
-#define vl  vector<ll>
-#define vpll  vector<pll>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define mp make_pair
-#define fi first
-#define pb push_back
-#define se second
-#define sqr(i) i * i
-#define turn_on(i, m) (m |= (1LL << i))
-#define turn_off(i, m) (m &= ~(1LL << i))
-#define getbit(i, m) ((m >> i) & 1)
-#define MASK(i) (1ll << (i))
-#define BIT(x, i) (((x) >> (i)) & 1) // lấy ra bit thứ i của số x
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
-// ((m >> i) & 1) : gia tri m co bit thu i bat
-// (m |= (1LL << i)) : bat bit thu i cua m len
-// (m &= ~(1LL << i)) : tat bit thu i cua m
-const int MAXN = 200000;
-const int LOG = 17;
-const ll MOD = 1e9 + 7;
-const ll INF = 3e18 + 7;
-const int N = 505;
-
 int n, m;
 int visited[MAXN], ans[MAXN];
 vector<int> adj[MAXN];
@@ -61,14 +28,5 @@ void solve() {
         topo.pop();
     }
     for(int i = 1; i <= n; i++) cout << ans[i] << " ";
-}
-
-int main(){ 
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    solve();
 }
 
