5d5aa11e0dc8698363ab00c605583e8f400ff47a
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-02-14 16:39:10 +0700

    Update ..cpp

--- ..cpp
+++ ..cpp
@@ -1,5 +1,3 @@
-// Minh Tú 
-
 #include <bits/stdc++.h>
 #pragma GCC("Ofast")
 #pragma GCC optimize("O3,unroll-loops")
@@ -13,8 +11,6 @@ using namespace std;
 #define pll  pair<ll, ll>
 #define vpii vector<pii>
 #define vpll  vector<pll>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define sz(x) x.size()
 #define mp make_pair
 #define pb push_back
 #define fi first
@@ -23,39 +19,36 @@ using namespace std;
 #define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
 #define turn_on(i, m) (m |= (1LL << i))
 #define turn_off(i, m) (m &= ~(1LL << i))
-#define MASK(i) (1ll << (i))
-#define BIT(x, i) (((x) >> (i)) & 1) 
-int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+#define BIT(x, i) (((x) >> (i)) & 1)
+
+#define PROBLEM "test"
+
+int dx[] = {-1, 0, 1, 0}; // U R D L
+int dy[] = {0, 1, 0, -1};
+const int mod[] = {(int)1e9 + 2277, (int)1e9 + 5277, (int)1e9 + 8277, (int)1e9+ 9277};
 const double eps = 1e-9;
-const int MAXN = 10000 + 77;
 const int LOG = 17;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 
-void solve() {
-    
+// ----------------------------------------
+
+void solve(){
+   
+}  
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    if (fopen(PROBLEM".inp", "r")){
+        freopen(PROBLEM".inp", "r", stdin);
+        freopen(PROBLEM".out", "w", stdout);
+    }
+    bool ok = false;
+    if(ok) {
+        int t; cin >> t;
+        while(t--)  solve();
+    }
+    else
+        solve();
 }
-
-int main(){ 
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    int t;
-    cin >> t;
-    while(t--) 
-    solve();
-}
-
-
-
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
-// (((x) >> i) & 1) : lay ra bit thu i cua x
-// (m |= (1LL << i)) : bat bit thu i cua m len
-// (m &= ~(1LL << i)) : tat bit thu i cua m
-// __builtin_clz(n)) : dem so luong so 0 dung dau 
-// __builtin_ctz(n)) : dem so luong so 0 dung cuoi
-// __builtin_popcount(n) : dem so luong so 1 trong bit
