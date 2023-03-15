a6b991505f9e57cc7637f440b8d3057453fd50b6
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-03-15 21:30:37 +0700

    Update HashString.cpp

--- HashString.cpp
+++ HashString.cpp
@@ -1,35 +1,3 @@
-#include <bits/stdc++.h>
-#pragma GCC("Ofast")
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-using namespace std;
-#define ll  long long
-#define db  double
-#define str string
-#define vl  vector<ll>
-#define pii pair<int, int>
-#define pll  pair<ll, ll>
-#define vpii vector<pii>
-#define vpll  vector<pll>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define sz(x) x.size()
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define sqr(i) i * i
-#define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
-#define turn_on(i, m) (m |= (1LL << i))
-#define turn_off(i, m) (m &= ~(1LL << i))
-#define MASK(i) (1ll << (i))
-#define BIT(x, i) (((x) >> (i)) & 1) 
-int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const double eps = 1e-9;
-const int LOG = 17;
-// const ll INF = 1e18;
-
-// ----------------------------------------
 const int maxn = 2e5 + 7;
 const int base = 44;
 ll mod[] = {(ll)1e9 + 2277, (ll)1e9 + 5277};
@@ -89,27 +57,5 @@ void solve() {
     for(int i = 0; i < n; i++) arr[i] = '.' + arr[i];
     for(int i = 0; i < n; i++) Build_Hash(arr[i], i);
 }
-
-int main(){ 
-    // clock_t tStart = clock();
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #else
-    freopen("thuyvan.inp", "r", stdin);
-    freopen("thuyvan.out", "w", stdout);
-    #endif
-    precalc();
-    bool ok = false;
-    if(ok) {
-        int t; cin >> t;
-        while(t--)  solve();
-    }   
-    else
-        solve();
-    // clog << "\nTime taken: "<<(((double)clock() - tStart)/CLOCKS_PER_SEC) << "s"; 
-}
-
 
 
