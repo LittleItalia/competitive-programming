49401e0964b4d2c30933a3dc3b278d4e44af1016
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-03-19 17:45:19 +0700

    Update Tarjan.cpp

--- Tarjan.cpp
+++ Tarjan.cpp
@@ -1,31 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-
-using ll = long long;
-using pll = pair<ll, ll>;
- using vl = vector<ll>;
-using vb = vector<bool>;
-using vpll = vector<pll>;
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
- 
-//pairs
-#define mp make_pair
-#define fi first
-#define se second
-#define MASK(i) (1LL << (i))
-
-//vectors
-#define pb push_back
-#define pf push_front
-// 1 << i :  2 ^ i
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
-const int MOD = 1e9 + 7;
-const ll INF = 1e18;
-const int MAXN = 300005;
-
 ll n, m, num[MAXN], low[MAXN], id = 1, scc = 0;
 vl adj[MAXN];
 bool onstack[MAXN];
@@ -68,11 +40,3 @@ void solve() {
     cout << scc << '\n';
 }   
 
-int main(){
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    solve();
-}
