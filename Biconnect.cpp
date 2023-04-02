8eeca6ddb33ed2357f77365ceea8fece7e0f5e16
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-04-02 16:50:41 +0700

    Update Biconnect.cpp

--- Biconnect.cpp
+++ Biconnect.cpp
@@ -1,33 +1,3 @@
-// thành phần song liên thông
-
-#include <bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-
-using ll = long long;
-using pll = pair<ll, ll>;
- using vl = vector<ll>;
-using vb = vector<bool>;
-using vpll = vector<pll>;
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
- 
-//pairs
-#define mp make_pair
-#define fi first
-#define se second
-#define MASK(i) (1LL << (i))
-
-//vectors
-#define pb push_back
-#define pf push_front
-// 1 << i :  2 ^ i
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
-const int MOD = 1e9 + 7;
-const ll INF = 1e18;
-const int MAXN = 30005;
-
 ll root[MAXN], active[MAXN], n, m;
 bool vt[MAXN];
 vl st, adj[MAXN];
@@ -71,13 +41,4 @@ void solve() {
         if(!vt[i])
             dfs(i);
     
-}
-
-int main(){
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    solve();
 }
