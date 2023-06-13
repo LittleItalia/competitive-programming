aeb04674b28c7b679f880d4fab8adb8f3e02f965
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-06-13 16:40:30 +0700

    Update Lazy Propagation.cpp

--- Lazy Propagation.cpp
+++ Lazy Propagation.cpp
@@ -1,36 +1,3 @@
-#include <bits/stdc++.h>
-#pragma GCC("Ofast")
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-using namespace std;
-#define ll  long long
-#define db  double
-#define str string
-#define vl  vector<ll>
-#define pii pair<int, int>
-#define pll  pair<ll, ll>
-#define vpii vector<pii>
-#define vpll  vector<pll>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define sz(x) x.size()
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define sqr(i) i * i
-#define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
-#define turn_on(i, m) (m |= (1LL << i))
-#define turn_off(i, m) (m &= ~(1LL << i))
-#define MASK(i) (1ll << (i))
-#define BIT(x, i) (((x) >> (i)) & 1) 
-int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const double eps = 1e-9;
-const int MAXN = 500000 + 77;
-const int LOG = 17;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e18;
-
 int n, m;
 ll seg[4 * MAXN], lazy[4 * MAXN], arr[MAXN];
 
@@ -99,11 +66,3 @@ void solve() {
     }
 }
 
-int main(){ 
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    solve();
-}
