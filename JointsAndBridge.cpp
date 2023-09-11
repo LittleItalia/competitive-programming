5bd274076968593016a32450706f95200e450ed5
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-09-11 12:17:56 +0700

    Update JointsAndBridge.cpp

--- JointsAndBridge.cpp
+++ JointsAndBridge.cpp
@@ -6,14 +6,18 @@ vl adj[maxn];
 void dfs(int u, int par) {
     int child = 0;
     num[u] = low[u] = ++timeDFS;
+    
     for(int v : adj[u]) {
-        if(v == par) 
-            continue;
+        if(v == par) continue;
+        
         if(!num[v]) {
             dfs(v, u);
             low[u] = min(low[u], low[v]);
+            
             if(low[v] == num[v]) bridge++;
+            
             child++;
+            
             if(u == par) {
                 if(child > 1) joint[u] = true;
             }
@@ -28,18 +32,19 @@ void dfs(int u, int par) {
 void solve() {
     memset(joint, false, sizeof(joint));
     cin >> n >> m;
+    
     for(int i = 1; i <= m; i++) {
-        ll u, v;
-        cin >> u >> v;
-        adj[u].pb(v);
-        adj[v].pb(u);
+        ll u, v; cin >> u >> v;
+        
+        adj[u].pb(v), adj[v].pb(u);
     }
     for(int i = 1; i <= n; i++)
-        if(!num[i])
-            dfs(i, i);
+        if(!num[i]) dfs(i, i);
+    
     ll cntJoint = 0;
     for(int i = 1; i <= n; i++) 
         cntJoint += joint[i];
+    
     cout << cntJoint << " " << bridge;
 }   
 
