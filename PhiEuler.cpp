d9385a3e5e8a4ee7c6e2cb85a7f20be6ff2d8f86
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-09-15 17:06:47 +0700

    Update PhiEuler.cpp

--- PhiEuler.cpp
+++ PhiEuler.cpp
@@ -1,8 +1,3 @@
-#include <bits/stdc++.h>
-using namespace;
-
-using ll = long long;
-
 ll phi(ll n) {  
     ll res = 0;
     for(int i = 2; i <= sqrt(n); i++) {
@@ -15,8 +10,4 @@ ll phi(ll n) {
     if(n > 1) 
         res -= res / n;
     return res;
-}
-
-int main() {
-
 }
