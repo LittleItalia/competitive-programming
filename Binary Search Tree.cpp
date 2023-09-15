48e03be17db5f6555da7d50726e44aebdf55d5cf
Little <108563712+LittleNoPro@users.noreply.github.com>
2023-09-15 17:05:20 +0700

    Update Binary Search Tree.cpp

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -1,43 +1,3 @@
-// Minh Tú 
-
-#include <bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-using namespace std;
-#define ll  long long
-#define db  double
-#define pll  pair<ll, ll>
-#define str string
-#define pii pair<int, int>
-#define vpii vector<pii>
-#define vl  vector<ll>
-#define vpll  vector<pll>
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define mp make_pair
-#define sz(x) x.size()
-#define fi first
-#define pb push_back
-#define se second
-#define sqr(i) i * i
-#define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
-#define turn_on(i, m) (m |= (1LL << i))
-#define turn_off(i, m) (m &= ~(1LL << i))
-#define MASK(i) (1ll << (i))
-#define BIT(x, i) (((x) >> (i)) & 1) 
-// x << y :  x * (2 ^ y) 
-// x >> y :  x / (2 ^ y)
-// (((x) >> i) & 1) : lay ra bit thu i cua x
-// (m |= (1LL << i)) : bat bit thu i cua m len
-// (m &= ~(1LL << i)) : tat bit thu i cua m
-// __builtin_clz(n)) : dem so luong so 0 dung dau 
-// __builtin_ctz(n)) : dem so luong so 0 dung cuoi
-// __builtin_popcount(n) : dem so luong so 1 trong bit
-int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-const int MAXN = 2e5 + 1000;
-const int LOG = 17;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e18;
 
 struct Node {
     int data; 
@@ -140,17 +100,4 @@ void inorder(Node* t) {
     inorder(t->left);
     cout << t->data << " ";
     inorder(t->right);
-}
-
-void solve() {
-    
-}
-
-int main(){ 
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    solve();
 }
